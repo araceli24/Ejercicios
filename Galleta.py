9905964b81cbf3f810db96f26bff15da0eb0326c
araceli24 <aracelilores@gmail.com>
2017-11-28 09:58:39 +0100

    Clase Galleta

---
+++ Galleta.py
@@ -0,0 +1,11 @@
+class Galleta:
+    	chocolate = False
+def __init__(self, sabor, color, radio):
+		self.sabor = sabor
+		self.color = color
+		self.radio = radio
+		print('acabas de crear una galleta {} {}'.format(sabor,color))
+def __len__(self):
+	return self.radio
+una_galleta=Galleta('dulce','marrón',5)
+len(una_galleta)
